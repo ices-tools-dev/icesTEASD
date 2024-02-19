8b4b43924444742f6da1cbd467db0dc0af870b63
Neil Maginnis <neilm@ices.dk>
2024-02-19 16:32:38 +0100

    Convert SID and SAG checks into a module

--- R/app_server.R
+++ R/app_server.R
@@ -9,41 +9,7 @@
 #' @noRd
 app_server <- function(input, output, session) {
 
-  output$year_selector <- renderUI({
-    years <- seq(lubridate::year(Sys.Date()),lubridate::year(Sys.Date())-2)
-     if(lubridate::month(Sys.Date()) <=5) {
-       default_year <- years[2]
-     } else {
-       default_year <- years[1]
-     }
-    shiny::selectInput(inputId = "year", label = "Select Assessment Year", choices = years, selected = default_year, multiple = F)
-  })
-
-  data <- reactive({
-    issues <-check_stock_db_errors(year = input$year)
-  }) %>%
-    bindEvent(input$check)
-
-
-  output$PO_table <- DT::renderDataTable({
-    req(!is.null(data()))
-    DT::datatable(data.frame(PO =  1:4), options = list(dom="t"),
-                  rownames = FALSE)
-
-  })
-
-  output$stock_table <- DT::renderDT({
-    req(!is.null(data()))
-    DT::datatable(data(), options = list(pageLength = 10,
-                                    lengthMenu = c(5, 10, 15, 20)),
-                  rownames = FALSE)
-  })
-
-
-  output$n_errors <- renderText({
-    req(!is.null(data()))
-    nrow(data())
-  })
-
+  mod_SID_SAG_checks_server("SID_SAG_checks_1")
   mod_user_checks_server("user_checks_1")
+  
 }
