1e82330f6462e5f3353e8f4d3c32c428a0f2d15c
Colin Millar <colin.millar@ices.dk>
2024-02-19 16:24:18 +0100

    whitespace

--- R/fct_check_db_errors.R
+++ R/fct_check_db_errors.R
@@ -1,4 +1,4 @@
-#' check_db_errors 
+#' check_db_errors
 #'
 #' @description A fct function
 #'
@@ -9,7 +9,7 @@
 #' @importFrom icesFO load_sid
 #' @importFrom icesSAG getListStocks
 #' @importFrom dplyr filter select mutate
-#' 
+#'
 check_stock_db_errors <- function(year) {
   
   sid_data <- load_sid(year)
@@ -20,19 +20,19 @@ check_stock_db_errors <- function(year) {
     shiny::need(!is.null(sag_data), "SAG not responding correctly")
   )
   sag_data <- sag_data %>% dplyr::filter(Purpose == "Advice")
-  
-  SID_errors <- sid_data %>% dplyr::filter(YearOfNextAssessment == year) %>% 
-    dplyr::select(Stock = StockKeyLabel) %>% 
+
+  SID_errors <- sid_data %>% dplyr::filter(YearOfNextAssessment == year) %>%
+    dplyr::select(Stock = StockKeyLabel) %>%
     dplyr::mutate(Issue = "Year of Next Assessment in past")
-  
+
   sid_data <- sid_data %>% dplyr::filter(YearOfLastAssessment == year)
 
-  mismatch_missing_in_SID <- data.frame(Stock = setdiff(sag_data$StockKeyLabel,sid_data$StockKeyLabel)) %>% 
+  mismatch_missing_in_SID <- data.frame(Stock = setdiff(sag_data$StockKeyLabel,sid_data$StockKeyLabel)) %>%
     dplyr::mutate(
                   Issue = "Stock missing from SID")
-  mismatch_missing_in_SAG <- data.frame(Stock = setdiff(sid_data$StockKeyLabel,sag_data$StockKeyLabel)) %>% 
+  mismatch_missing_in_SAG <- data.frame(Stock = setdiff(sid_data$StockKeyLabel,sag_data$StockKeyLabel)) %>%
     dplyr::mutate(Issue = "Stock missing from SAG")
-  
+
 
   return(dplyr::bind_rows(SID_errors,
               mismatch_missing_in_SID,
