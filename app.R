dbdfae9ee00f86fd6c62a2017ba927e1bc87ae52
Colin Millar <colin.millar@ices.dk>
2024-02-19 20:41:31 +0100

    getting hacky ;)

--- app.R
+++ app.R
@@ -1,10 +1,13 @@
 # Launch the ShinyApp
 
-options(shiny.autoload.r = FALSE)
-
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
 options("golem.app.prod" = TRUE)
 
 print("hello")
+cat(capture.output(search()))
+cat(ls())
 
-run_app_icesTEASD() # add parameters here (if any)
+#run_app_icesTEASD() # add parameters here (if any)
+
+x <- get("run_app_icesTEASD", pos = 1)
+x()
