8707f1dd6f859e5dbc3d7995262a8058ae11d0fc
Neil Maginnis <neilm@ices.dk>
2024-09-16 12:10:49 +0200

    merge replaced_SAG_advice fix into new_layout and SID filtering. version bump

--- R/mod_db_checks.R
+++ R/mod_db_checks.R
@@ -90,7 +90,7 @@ mod_db_checks_server <- function(id){
     checks_data <- reactive({
       req(!is.null(stock_data()))
       issues <-check_stock_db_errors(stock_data()$SID_data,
-                                     stock_data()$SAG_data,
+                                     stock_data()$SAG_data_raw,
                                      stock_data()$ASD_data,
                                      year = selected_year())
     }) %>% bindEvent(input$check)
