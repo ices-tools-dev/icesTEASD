d6052b157abe892ed36911ae93a428ced5a0a28b
Neil Maginnis <neil.maginnis@ices.dk>
2025-01-28 18:16:19 +0100

    process powerBI advice date input

--- data-raw/advice_releases.R
+++ data-raw/advice_releases.R
@@ -4,9 +4,27 @@ library(readxl)
 library(lubridate)
 library(dplyr)
 
-path <- "data-raw/teasd_adg_advice_dates.xlsx"
-advice_releases <- read_xlsx(path = path, na = "NA", col_types = c("date", "text", "text", "text")) %>% 
-  filter(!is.na(ADG)) %>% 
-  mutate(advice_release_date = lubridate::date(advice_release_date))
+# path <- "data-raw/teasd_adg_advice_dates.xlsx"
+# 
+# advice_releases <- read_xlsx(path = path, na = "NA", col_types = c("date", "text", "text", "text")) %>% 
+#   filter(!is.na(ADG)) %>% 
+#   mutate(advice_release_date = lubridate::date(advice_release_date))
+# 
+# usethis::use_data(advice_releases, overwrite = TRUE)
+# 
+# 
+# 
+
+raw_data <- read_xlsx("data-raw/data.xlsx")
+web_conf <- raw_data[grep("ACOM web" , raw_data$`Group Name`),c(1,2,7)]
+web_conf <- web_conf %>% mutate(ADG = gsub(pattern = "WC",replacement = "ADG", .data$`Meeting Name`))
+web_conf <- web_conf %>% mutate(ADG = gsub(pattern = " 2025",replacement = "", .data$`ADG`))
+names(web_conf)[3] <- "advice_release_date"
+writexl::write_xlsx(web_conf[,-2], col_names = T, path = "data-raw/web_conf_dates.xlsx")
+
+advice_releases <- web_conf %>% 
+    filter(!is.na(ADG)) %>%
+    mutate(advice_release_date = lubridate::dmy(advice_release_date))
 
 usethis::use_data(advice_releases, overwrite = TRUE)
+
