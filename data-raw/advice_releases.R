b0ff458318e61f5960341134417b44fbc4b3ce31
Neil Maginnis <neil.maginnis@ices.dk>
2026-04-16 10:11:33 +0200

    Update remove duplicates from advice_releases

--- data-raw/advice_releases.R
+++ data-raw/advice_releases.R
@@ -39,8 +39,8 @@ library(stringr)
 advice_releases <- read_xlsx("data-raw/ADGs2026.xlsx") %>% 
     mutate(ADG = str_split_i(.data$`Meeting Name`, " ", i = 1),
            advice_release_date = lubridate::ymd(stringr::str_replace_all(.data$`Meeting End Date`, pattern = "/", replacement = "-"))) %>%
-  select(c("Request Topic", "ADG", "advice_release_date"))
-
+  select(c("ADG", "advice_release_date"))
+advice_releases <- advice_releases[!duplicated(advice_releases),]
 
 
 usethis::use_data(advice_releases, overwrite = TRUE)
